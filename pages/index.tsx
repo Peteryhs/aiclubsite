5272aaec6046a4b8ab64417241e79dda3bfac096
petershao288 <petershao288@gmail.com>
2024-10-03 21:31:43 +0000

    modified:   ../pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import { LandingPage } from "@/components/landing-page"
-
+import { LandingPage } from "@/components/landing-page";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <LandingPage />
+    <>
+      <Head>
+        <title>IRHS AI Club</title>
+        <meta name="description" content="Description of your site" />
+      </Head>
+      <LandingPage />
+    </>
   );
 }
